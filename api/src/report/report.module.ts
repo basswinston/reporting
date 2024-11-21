73a0afc97350f4dd4c1e2b257d53404a00466da1
Ben Keys <bkeys818@gmail.com>
2024-11-21 14:17:08 -0500

    🐛 Add related TypeORM entities to module imports

--- api/src/report/report.module.ts
+++ api/src/report/report.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
-import { ReportService } from './report.service';
-import { ReportController } from './report.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Report } from "./report";
+import { Module } from '@nestjs/common'
+import { ReportService } from './report.service'
+import { ReportController } from './report.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Report } from './report'
+import { User } from '../user/user'
+import { Entity } from '../entity/entity'
+import { Incident } from '../incident/incident'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Report])],
-  exports: [TypeOrmModule],
-  controllers: [ReportController],
-  providers: [ReportService],
+    imports: [TypeOrmModule.forFeature([Report, User, Entity, Incident])],
+    exports: [TypeOrmModule],
+    controllers: [ReportController],
+    providers: [ReportService],
 })
 export class ReportModule {}
--- api/src/user/user.module.ts
+++ api/src/user/user.module.ts
@@ -3,9 +3,10 @@ import { UserService } from './user.service'
 import { UserController } from './user.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { User } from './user'
+import { Report } from '../report/report'
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
+    imports: [TypeOrmModule.forFeature([User, Report])],
     controllers: [UserController],
     providers: [UserService],
 })
