37540daa28be44ab6c1db2220ace9ce0cd63dfad
Ben Keys <bkeys818@gmail.com>
2024-11-21 09:12:00 -0500

    Added DTO and updated functions to match others
    commit a8fcaf98fe7e7ccef5564a9026960a956308f894
    Author: Cole Snyder <MidLanerST@protonmail.com>
    Date:   Wed Nov 20 15:42:28 2024 -0700

--- api/src/incident/incident.service.ts
+++ api/src/incident/incident.service.ts
@@ -1,13 +1,9 @@
-/*import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class IncidentService {}
-*/
-
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Incident } from './incident'
 import { DeleteResult, Repository } from 'typeorm'
+import { CreateIncidentDto } from './dto/create-incident-dto'
+import { UpdateIncidentDto } from './dto/update-incident-dto'
 
 @Injectable()
 export class IncidentService {
@@ -20,11 +16,11 @@ export class IncidentService {
     async getAllIncidents(): Promise<Incident[]> {
         // we can now just call straight to our repo, using its pre-defined methods
         return await this.repo.find({
-            relations: {
-                // reports: {
-                //     report_user: true
-                // }
-            },
+            //relations: {
+            //    reports: {
+            //        report_user: true
+            //    }
+            //}
         })
     }
 
@@ -36,11 +32,11 @@ export class IncidentService {
                 where: {
                     incidentId: idToFind,
                 },
-                relations: {
-                    // reports: {
-                    //     report_user: true
-                    // }
-                },
+                //relations: {
+                //    reports: {
+                //        report_user: true
+                //    }
+                //}
             })
             .catch(() => {
                 throw new HttpException(
@@ -50,55 +46,19 @@ export class IncidentService {
             })
     }
 
-    // create one
-    async createIncident(newIncident: Incident): Promise<Incident> {
-        await this.repo
-            .exists({
-                where: {
-                    incidentId: newIncident.incidentId,
-                },
-            })
-            .then((exists) => {
-                if (exists)
-                    throw new HttpException(
-                        `Incident with ID ${newIncident.incidentId} already exists!`,
-                        HttpStatus.BAD_REQUEST,
-                    )
-            })
-
+    // creates a new Incident
+    async createIncident(data: CreateIncidentDto) {
+        const newIncident = this.repo.create(data)
         return await this.repo.save(newIncident)
     }
 
-    // update one
-    async updateIncident(routeId: number, incidentToUpdate: Incident) {
-        // checking if the route ID and the one in the body match
-        if (routeId != incidentToUpdate.incidentId) {
-            throw new HttpException(
-                `Route ID and Body ID do not match!`,
-                HttpStatus.BAD_REQUEST,
-            )
-        }
-
-        // checking that the Incident we want to update exists in the database already
-        // if it doesn't we'd create a new one, which we don't want
-        await this.repo
-            .exists({
-                where: {
-                    incidentId: incidentToUpdate.incidentId,
-                },
-            })
-            .then((exists) => {
-                if (!exists)
-                    throw new HttpException(
-                        `Incident with ID ${incidentToUpdate.incidentId} does not exist!`,
-                        HttpStatus.NOT_FOUND,
-                    )
-            })
-
-        return await this.repo.save(incidentToUpdate)
+    // update given Incident
+    async updateIncident(incident: Incident, updateData: UpdateIncidentDto) {
+        const updatedIncident = this.repo.merge(incident, updateData)
+        return await this.repo.save(updatedIncident)
     }
 
-    // delete one
+    // delete given Incident
     async deleteIncident(id: number): Promise<DeleteResult> {
         return await this.repo.delete(id)
     }
--- api/src/incident/incident.ts
+++ api/src/incident/incident.ts
@@ -1,36 +1,30 @@
 import { Column, Entity, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
-import { IsNotEmpty, IsNumber, IsString, IsDate } from 'class-validator'
 import { Report } from '../report/report'
 
 @Entity()
 export class Incident {
-    // PK
     @PrimaryGeneratedColumn()
-    @IsNotEmpty()
-    @IsNumber()
     incidentId: number
 
     @Column()
-    @IsDate()
     incidentDate: Date
 
     @Column()
-    @IsString()
-    //@Length()
     incidentLocation: String
 
     @Column()
-    @IsString()
     relatedAccounts: String
 
     @Column()
-    @IsString()
     transactionDetails: String
 
     @Column()
-    @IsString()
     description: String
 
-    @OneToOne(() => Report)
+    /**
+     * Each report has a single Incident it is reporting,
+     * leading to a 1-1 relationship
+     */
+    @OneToOne(() => Report, (report) => report.incidentId)
     reports: Report
 }
